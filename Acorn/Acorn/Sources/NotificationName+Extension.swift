0d53830b218b529bda170b8d48120d864172ffc6
전소영 <jsu3417@gmail.com>
2022-09-12 12:12:15 +0900

    [#1] Notification+Extension memoryWarning추가

---
+++ Acorn/Acorn/Sources/NotificationName+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationName+Extension.swift
+//  Acorn
+//
+//  Created by 전소영 on 2022/09/10.
+//
+
+import Foundation
+import UIKit
+
+public extension Notification.Name {
+    static let memoryWarning = UIApplication.didReceiveMemoryWarningNotification
+}
