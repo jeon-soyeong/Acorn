edba9ebae261b9b0620b2a1a79fdac66e6dc4b1c
전소영 <jsu3417@gmail.com>
2022-09-12 12:12:51 +0900

    [#1] DiskCache 추가

---
+++ Acorn/Acorn/Sources/DiskCache.swift
@@ -0,0 +1,73 @@
+//
+//  DiskCache.swift
+//  Acorn
+//
+//  Created by 전소영 on 2022/09/12.
+//
+
+import Foundation
+
+class DiskCache {
+    public static let shared = DiskCache()
+    
+    private var fileDirectoryURL: URL? {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent("cache")
+    }
+    
+    init() {
+        createFileDirectory()
+    }
+    
+    func read(with key: String) -> CachedImage? {
+        guard let fileURL = getFileURL(key: key) else {
+            print(ImageCacheError.invalidFileURL.description)
+            return nil
+        }
+        guard let imageData = try? Data(contentsOf: fileURL) else {
+            print(ImageCacheError.failedLoadDataFromDisk.description)
+            return nil
+        }
+        return CachedImage(imageData: imageData)
+    }
+    
+    func save(data: CachedImage, with key: String) {
+        guard let fileURL = getFileURL(key: key) else {
+            print(ImageCacheError.invalidFileURL.description)
+            return
+        }
+        do {
+            try data.imageData.write(to: fileURL)
+        } catch {
+            print(ImageCacheError.failedWriteDataToDisk.description)
+            return
+        }
+    }
+   
+    func clearDiskCache() {
+        if let fileDirectoryURL = self.fileDirectoryURL {
+            try? FileManager.default.removeItem(at: fileDirectoryURL)
+        }
+        self.createFileDirectory()
+    }
+    
+    private func createFileDirectory() {
+        guard let fileDirectoryURL = self.fileDirectoryURL else {
+            print(ImageCacheError.invalidFileDirectoryURL.description)
+            return
+        }
+        if FileManager.default.fileExists(atPath: fileDirectoryURL.path) { return }
+        try? FileManager.default.createDirectory(atPath: fileDirectoryURL.path, withIntermediateDirectories: true, attributes: nil)
+    }
+    
+    private func getFileURL(key: String) -> URL? {
+        guard let fileName = URL(string: key)?.lastPathComponent else {
+            print(ImageCacheError.invalidFileName.description)
+            return nil
+        }
+        guard let fileDirectoryURL = self.fileDirectoryURL else {
+            print(ImageCacheError.invalidFileDirectoryURL.description)
+            return nil
+        }
+        return fileDirectoryURL.appendingPathComponent(fileName)
+    }
+}
