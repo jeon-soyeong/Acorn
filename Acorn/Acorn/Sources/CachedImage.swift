a5a110628ee55a39d0f3d5ddfbecf37f862b8e53
전소영 <jsu3417@gmail.com>
2022-09-12 12:10:04 +0900

    [#1] CachedImage 추가

---
+++ Acorn/Acorn/Sources/CachedImage.swift
@@ -0,0 +1,16 @@
+//
+//  CachedImage.swift
+//  Acorn
+//
+//  Created by 전소영 on 2022/09/09.
+//
+
+import Foundation
+
+public class CachedImage {
+    let imageData: Data
+    
+    init(imageData: Data) {
+        self.imageData = imageData
+    }
+}
