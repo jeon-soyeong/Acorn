00f4c16a687f14ba5be347409dac7cb99cdd5a9d
전소영 <jsu3417@gmail.com>
2022-09-14 16:52:05 +0900

    [#1] ImageCacheManager private, public 분리

--- Acorn/Acorn/Sources/ImageCacheManager.swift
+++ Acorn/Acorn/Sources/ImageCacheManager.swift
@@ -23,12 +23,18 @@ public class ImageCacheManager {
         NotificationCenter.default.removeObserver(self, name: .willTerminate, object: nil)
         NotificationCenter.default.removeObserver(self, name: .didEnterBackground, object: nil)
     }
+}
 
-    public func configureCache(maximumMemoryBytes: Int? = CacheConstants.maximumMemoryBytes, maximumDiskBytes: Int? = CacheConstants.maximumDiskBytes) {
+// MARK: public
+public extension ImageCacheManager {
+    func configureCache(maximumMemoryBytes: Int? = CacheConstants.maximumMemoryBytes,
+                               maximumDiskBytes: Int? = CacheConstants.maximumDiskBytes,
+                               expiration: CacheExpiration? = .days(10)) {
         if let maximumMemoryBytes = maximumMemoryBytes,
-           let maximumDiskBytes = maximumDiskBytes {
+           let maximumDiskBytes = maximumDiskBytes,
+           let expiration = expiration {
             memoryCache = MemoryCache(maximumMemoryBytes: maximumMemoryBytes)
-            diskCache = DiskCache(maximumDiskBytes: maximumDiskBytes)
+            diskCache = DiskCache(maximumDiskBytes: maximumDiskBytes, expiration: expiration)
         }
         
         //TODO: Test 후 삭제
@@ -36,7 +42,7 @@ public class ImageCacheManager {
 //        print("clearDiskCache")
     }
 
-    public func readCachedImageData(key: String) -> CachedImage? {
+    func readCachedImageData(key: String) -> CachedImage? {
         guard let memoryCachedImageData = readMemoryCachedImageData(with: key) else {
             guard let diskCachedImageData = readDiskCachedImageData(with: key) else {
                 return nil
@@ -48,10 +54,10 @@ public class ImageCacheManager {
         return memoryCachedImageData
     }
 
-    public func downloadImageData(key: String, completionHandler: @escaping (CachedImage?) -> Void) -> URLSessionDataTask? {
+    func downloadImageData(key: String, completionHandler: @escaping (CachedImage?) -> Void) -> URLSessionDataTask? {
         let dataTask = downloadImageData(url: key) { downloadImageData in
            guard let downloadImageData = downloadImageData else {
-               print(ImageCacheError.failedDownloadImageData.description)
+               debugPrint(ImageCacheError.failedDownloadImageData.description)
                return
            }
            print("downloadImageData: \(downloadImageData.imageData)")
@@ -61,16 +67,31 @@ public class ImageCacheManager {
         return dataTask
     }
 
-    public func cancelImageDownloadTask() {
+    func cancelImageDownloadTask() {
         dataTask?.cancel()
         dataTask = nil
     }
+    
+    @objc func clearMemoryCache() {
+        memoryCache?.clearMemoryCache()
+    }
 
-    private func readMemoryCachedImageData(with key: String) -> CachedImage? {
+    @objc func clearDiskCache() {
+        diskCache?.clearDiskCache()
+    }
+    
+    @objc func removeOldDiskCache() {
+        diskCache?.removeOldCache()
+    }
+}
+
+// MARK: private
+private extension ImageCacheManager {
+    func readMemoryCachedImageData(with key: String) -> CachedImage? {
         return memoryCache?.read(with: key)
     }
 
-    private func readDiskCachedImageData(with key: String) -> CachedImage? {
+    func readDiskCachedImageData(with key: String) -> CachedImage? {
         let cachedImage = diskCache?.read(with: key)
         if let cachedImage = cachedImage {
             memoryCache?.save(data: cachedImage, with: key)
@@ -78,21 +99,21 @@ public class ImageCacheManager {
         return cachedImage
     }
 
-    private func saveMemoryCachedImageData(data: CachedImage, with key: String) {
+    func saveMemoryCachedImageData(data: CachedImage, with key: String) {
         print("saveMemoryCachedImageData")
         memoryCache?.save(data: data, with: key)
     }
 
-    private func saveDiskCachedImageData(data: CachedImage, with key: String) {
+    func saveDiskCachedImageData(data: CachedImage, with key: String) {
         print("saveDiskCachedImageData")
         diskCache?.save(data: data, with: key)
     }
 
-    private func downloadImageData(url: String, completionHandler: @escaping (CachedImage?) -> Void) -> URLSessionDataTask? {
+    func downloadImageData(url: String, completionHandler: @escaping (CachedImage?) -> Void) -> URLSessionDataTask? {
         if let imageUrl = URL(string: url) {
             dataTask = URLSession.shared.dataTask(with: imageUrl) { [weak self] (data, response, error) in
                 guard let data = data else {
-                    print(ImageCacheError.failedData.description)
+                    debugPrint(ImageCacheError.failedData.description)
                     return
                 }
                 self?.saveMemoryCachedImageData(data: CachedImage(imageData: data), with: url)
@@ -104,23 +125,9 @@ public class ImageCacheManager {
         return dataTask
     }
 
-    private func setupNotification() {
+    func setupNotification() {
         NotificationCenter.default.addObserver(self, selector: #selector(clearMemoryCache), name: .didReceiveMemoryWarning, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(removeOldDiskCache), name: .willTerminate, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(removeOldDiskCache), name: .didEnterBackground, object: nil)
-    }
-
-    @objc private func clearMemoryCache() {
-        memoryCache?.clearMemoryCache()
-    }
-
-    //TODO: 사용할지
-    @objc private func clearDiskCache() {
-        diskCache?.clearDiskCache()
-    }
-
-    @objc private func removeOldDiskCache() {
-        diskCache?.removeExpiredValues()
-        diskCache?.removeSizeExceededValues()
     }
 }
